e72359ac7056989259efd249473d4456c54ce7e1
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-02-25 17:45:36 -0500

    [class.abc], [ngClass], [class]

--- src/app/pais/pages/region/region.component.ts
+++ src/app/pais/pages/region/region.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-region',
   templateUrl: './region.component.html',
-  styles: [
+  styles: [`
+    button {
+      margin-right: 5px;
+    }  
+  `
   ]
 })
-export class RegionComponent implements OnInit {
+export class RegionComponent {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionSeleccionada: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  seleccionarRegion(region: string): void {
+    this.regionSeleccionada = region;
+    //TODO: Hacer llamado al servicio
 
+  }
+  
+  getClaseCSSBtnRegion(region: string): string {
+    return region === this.regionSeleccionada ? 'btn btn-primary' : 'btn btn-outline-primary';
+  }
 }
